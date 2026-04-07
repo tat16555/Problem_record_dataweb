ae32960e726429b2167946a51d43e11c6d674818
tat16555 <pannatat.b@gmail.com>
2026-04-07 16:23:46 +0700

    first commit

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const API = "https://tat16555.github.io/Problem_record_dataweb/";
+const API = "https://script.google.com/macros/s/https://tat16555.github.io/Problem_record_dataweb//exec";
 
 let allData = [];
 
